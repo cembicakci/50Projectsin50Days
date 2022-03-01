1def3aa35589cb2b343c6bf655f7ffa549be866c
Cem Bıçakcı <73403359+cembicakci@users.noreply.github.com>
2022-03-01 23:20:22 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-const toggles = document.querySelectorAll(".faq-toggle");
-
-toggles.forEach((item) => {
-    item.addEventListener("click", () => {
-        item.parentNode.classList.toggle("active");
-    })
-})
